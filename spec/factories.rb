51a9b18464966530fcaec449ada8a2c2ebabb77e
Harley Alexander <baffleinc@gmail.com>
2013-01-09 16:53:50 +1100

    Finishing user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Harel Tank"
+    email    "harley@sertepernt.com"
+    password "Charlie123"
+    password_confirmation "Charlie123"
+  end
+end
