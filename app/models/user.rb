8431589a105cb34153522145808f2a0430d517e5
Harley Alexander <baffleinc@gmail.com>
2013-01-11 14:08:40 +1100

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,13 @@
 #  updated_at      :datetime         not null
 #  password_digest :string(255)
 #  remember_token  :string(255)
+#  admin           :boolean          default(FALSE)
 #
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
   
   before_save { self.email.downcase! }
   before_save :create_remember_token
@@ -25,6 +27,11 @@ class User < ActiveRecord::Base
              uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+  
+  def feed
+    #This is preliminary. See "Following users" for the full implimentation
+    Micropost.where("user_id = ?", id)
+  end
   
   private
     
